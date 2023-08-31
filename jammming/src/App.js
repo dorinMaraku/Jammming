9d5b419f63d8029d724dbd08f5b0523635137f35
dorinMaraku <dorinmaraku@gmail.com>
2023-08-31 18:29:29 +0200

    minor

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -139,6 +139,7 @@ export default function App() {
       setPlaylistURIs(prevPlaylistURIs => ([
         ...prevPlaylistURIs,
         newURI]))
+      setPlaylistStatus(prevStatus => !prevStatus)
     }
   }
   // console.log(playlistTracks)
